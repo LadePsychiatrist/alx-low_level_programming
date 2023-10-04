aa968a3c9a8f1dab6b98834a4a7dc42e8ede5c58
LadePsychiatrist <bolaleye865@gmail.com>
2023-10-04 03:07:58 -0700

    stroll

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,98 +1,77 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * get_word_count - Count the number of words in a string.
- * @str: The input string.
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
  *
- * Return: The number of words.
+ * Return: number of words
  */
-static int get_word_count(char *str)
+int count_word(char *s)
 {
-	int count = 0;
-	int len = strlen(str);
-	int i;
+	int flag, c, w;
 
-	for (i = 0; i < len; i++)
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
 	{
-	if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-	{
-	count++;
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
 	}
-	}
-	return (count);
+
+	return (w);
 }
 /**
- * allocate_and_copy - Allocate memory for a word and copy it.
- * @start: The starting index of the word.
- * @end: The ending index of the word.
- * @str: The input string.
+ * **strtow - splits a string into words
+ * @str: string to split
  *
- * Return: A pointer to the allocated memory.
- */
-static char *allocate_and_copy(int start, int end, char *str)
-{
-	int word_len = end - start + 1;
-	char *word = malloc((word_len + 1) * sizeof(char));
-
-	if (word == NULL)
-	{
-	return (NULL);
-	}
-	strncpy(word, &str[start], word_len);
-	word[word_len] = '\0';
-	return (word);
-}
-/**
- * strtow - Splits a string into words.
- * @str: The input string.
- *
- * Return: A pointer to an array of strings (words), or NULL on failure.
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
 char **strtow(char *str)
 {
-	if (str == NULL || *str == '\0')
-	{
-	return (NULL);
-	}
-	int len = strlen(str);
-	int word_count = get_word_count(str);
-	char **words = malloc((word_count + 1) * sizeof(char *));
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	if (words == NULL)
-	{
-	return (NULL);
-	}
-	words[word_count] = NULL;
-	if (word_count == 0)
-	{
-	return (words);
-	}
-	int count = 0, start = 0, end = 0;
-	int i;
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
 
-	for (i = 0; i < len; i++)
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
 	{
-	if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-	{
-	start = i;
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
 	}
-	if (str[i] != ' ' && (i == len - 1 || str[i + 1] == ' '))
-	{
-	end = i;
-	words[count] = allocate_and_copy(start, end, str);
-	if (words[count] == NULL)
-	{
-	while (--count >= 0)
-	{
-	free(words[count]);
-	}
-	free(words);
-	return (NULL);
-	}
-	count++;
-	}
-	}
-	return (words);
+
+	matrix[k] = NULL;
+
+	return (matrix);
 }
